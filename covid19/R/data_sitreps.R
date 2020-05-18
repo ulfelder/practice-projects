1b3fcafdd2442fb71ebb22b1fdae7de67c51807c
Jay Ulfelder <ulfelder@gmail.com>
2020-05-18 10:34:30 -0400

    Update data_sitreps.R
    Fixed issue with regex of report links in sitrep scraping script

--- covid19/R/data_sitreps.R
+++ covid19/R/data_sitreps.R
@@ -1,11 +1,12 @@
 require(tidyverse)
 require(rvest)
+require(lubridate)
 
 # get links to WHO situation reports and give them pubdates as names
 sitreps <- read_html("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/") %>%
   html_nodes("a") %>%
   html_attr("href") %>%
-  grep("[0-9]{8}-sitrep-[0-9]{1,3}", ., value = TRUE) %>%
+  grep("-sitrep-", ., value = TRUE) %>%
   sprintf("https://www.who.int%s", .) %>%
   unique(.)
 
